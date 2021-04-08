9b9f585dd1124509bc40833b3c36db4106c344f4
Shuhab Siddigi <shuhabsiddigi@pop-os.localdomain>
2021-04-08 17:06:51 +0200

    added schematic for dm24004

--- Watering-Plant-System.X/nbproject/Makefile-variables.mk
+++ Watering-Plant-System.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Watering-Plant-System.X.production.
-CND_ARTIFACT_PATH_default=dist/default/production/Watering-Plant-System.X.production.
+CND_ARTIFACT_NAME_default=Watering-Plant-System.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Watering-Plant-System.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=watering-plant-system.x.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/watering-plant-system.x.tar
